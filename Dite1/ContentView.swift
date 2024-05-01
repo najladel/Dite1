29e3a38406be773800b186a797eff7d7b38edf93
manal <manal.ml@icloud.com>
2024-05-02 01:49:32 +0300

    Initial Commit

---
+++ Dite1/ContentView.swift
@@ -0,0 +1,19 @@
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image("Dite")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dite1/Dite1App.swift
@@ -0,0 +1,17 @@
+//
+//  Dite1App.swift
+//  Dite1
+//
+//  Created by Manal Qaysi on 23/10/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct Dite1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
